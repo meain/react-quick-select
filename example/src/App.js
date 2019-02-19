453a230ff8173395e8ecaf0bb13aa03fd3ee6240
Abin Simon <abinsimon10@gmail.com>
2019-02-19 18:26:48 +0530

    ui working

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-quick-select'
+import QuickSelect from "react-quick-select";
+import numberToWords from "number-to-words";
+
+const range = (lowEnd, highEnd) => {
+  let arr = [];
+  while (lowEnd <= highEnd) {
+    arr.push(lowEnd++);
+  }
+  return arr;
+};
 
 export default class App extends Component {
-  render () {
+  optionRenderer(option) {
+    return <b style={{ margin: "0 10px" }}>{option.label}</b>;
+  }
+
+  colorRenderer(option) {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{ color: option.label, padding: "5px 30px" }}>
+        {option.label}
       </div>
-    )
+    );
+  }
+
+  render() {
+    const numberOptions = range(1, 100).map(number => {
+      return { value: number, label: numberToWords.toWords(number) };
+    });
+
+    const colorOptions = [
+      "red",
+      "green",
+      "blue",
+      "yellow",
+      "pink",
+      "black"
+    ].map(color => {
+      return { value: color, label: color };
+    });
+
+    return (
+      <div className="App">
+        <div>
+          My favorite number is
+          <QuickSelect
+            options={numberOptions}
+            selected={0}
+            height='500px'
+            width='300px'
+            trigger={this.optionRenderer}
+          />
+          and my favorite color is
+          <QuickSelect
+            options={colorOptions}
+            selected={0}
+            trigger={this.optionRenderer}
+            renderer={this.colorRenderer}
+          />
+        </div>
+      </div>
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,83 @@
 /**
- * @class ExampleComponent
+ * @class QuickSelect
  */
 
-import * as React from 'react'
+import * as React from "react";
 
-import styles from './styles.css'
+import styles from "./styles.css";
 
-export type Props = { text: string }
+export type Option = { value: string; label: string };
+export type Props = {
+  selected: number;
+  options: Option[];
+  height?: string;
+  width?: string;
+  renderer: (option: Option) => React.ReactNode;
+  trigger?: (option: Option) => React.ReactNode;
+};
+type State = {
+  hover: boolean;
+};
 
-export default class ExampleComponent extends React.Component<Props> {
+export default class QuickSelect extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hover: false };
+  }
+
+  defaultRenderer(option: Option, trigger = false) {
+    return (
+      <div className={trigger ? styles.defaulttrigger : styles.defaultitem}>
+        {option.label}
+      </div>
+    );
+  }
+
+  toggleHover(hover: boolean) {
+    this.setState({ ...this.state, hover });
+  }
+
   render() {
-    const {
-      text
-    } = this.props
+    let {
+      options,
+      selected = 0,
+      renderer = this.defaultRenderer,
+      trigger,
+      height = "200px",
+      width
+    } = this.props;
+
+    // const clname = updateMaxHeight(height, styles);
+
+    const defaultTrigger = (option: Option) =>
+      this.defaultRenderer(option, true);
+    trigger = trigger || defaultTrigger;
+
+    const { hover } = this.state;
+
+    const style: React.CSSProperties = {};
+    if (hover) {
+      style.maxHeight = height;
+      if (width) style.width = width;
+    }
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div
+        className={styles.wrapper}
+        onMouseEnter={() => this.toggleHover(true)}
+        onMouseLeave={() => this.toggleHover(false)}
+      >
+        <div className={styles.trigger}>
+          {trigger(options[selected]) || renderer(options[selected])}
+        </div>
+        <div className={styles.items} style={style}>
+          {options.map((option, i) => (
+            <div key={i} className={styles.item}>
+              {renderer(option)}
+            </div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
